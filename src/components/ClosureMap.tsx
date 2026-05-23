1adfb365db127314b2521f0bf5c008bfc282f7b1
Marty McGee <mcgee.marty@gmail.com>
2026-05-23 12:35:01 -0700

    v0.4.9: 511.org Leaflet OpenStreetMap Simple Default/Master Map

--- src/components/ClosureMap.tsx
+++ src/components/ClosureMap.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { useRouter } from 'next/navigation';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+// import 'leaflet/dist/leaflet.css';
 
 // Fix for default marker icons
 delete (L.Icon.Default.prototype as any)._getIconUrl;
--- src/components/map/masterMap.tsx
+++ src/components/map/masterMap.tsx
@@ -4,7 +4,7 @@
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+// import 'leaflet/dist/leaflet.css';
 
 // Fix for default marker icons
 delete (L.Icon.Default.prototype as any)._getIconUrl;
